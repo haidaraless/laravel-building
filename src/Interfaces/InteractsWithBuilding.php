4c1fbc0ced19a304930020999af80ee3ba9be294
haidarhalessa <haidarhalessa@users.noreply.github.com>
2024-07-10 07:30:41 +0000

    Fix styling

--- src/Interfaces/InteractsWithBuilding.php
+++ src/Interfaces/InteractsWithBuilding.php
@@ -7,7 +7,10 @@ use Illuminate\Database\Eloquent\Relations\HasOne;
 interface InteractsWithBuilding
 {
     public function building(): HasOne;
+
     public function getAllFloors(int $buildingId): array;
+
     public function getAllUnits(int $buildingId): array;
+
     public function getAllSpaces(int $buildingId): array;
 }
